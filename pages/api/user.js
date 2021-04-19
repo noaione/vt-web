4b3c42c1f1590a42c5e5968595e0a7ab80043a48
noaione <noaione0809@gmail.com>
2021-04-19 17:23:31 +0700

    i s2g

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,6 +1,5 @@
 import nextConnect from "next-connect";
 import auth from "../../middleware/auth";
-import { deleteUser, updateUserByUsername } from "../../lib/db";
 
 const handler = nextConnect();
 
