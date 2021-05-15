3f6fee1b32ec5e86be71a6c45df9930ea548a966
noaione <noaione0809@gmail.com>
2021-05-15 15:11:07 +0700

    fetcher: pass httpCode

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,6 +1,7 @@
 interface ExtendedError extends Error {
     response: Response;
     data: any;
+    httpCode: number;
 }
 
 export default async function fetcher(...args: Parameters<typeof fetch>) {
@@ -14,11 +15,15 @@ export default async function fetcher(...args: Parameters<typeof fetch>) {
         const error = new Error(response.statusText) as ExtendedError;
         error.response = response;
         error.data = data;
+        error.httpCode = response.status;
 
         throw error;
     } catch (error) {
         if (!error.data) {
             error.data = { message: error.message };
+        }
+        if (typeof error.httpCode !== "number") {
+            error.httpCode = 500;
         }
         throw error;
     }
