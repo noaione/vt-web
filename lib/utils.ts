b986408b217cf65ac6cedab8728916a8e5a3064e
noaione <noaione0809@gmail.com>
2021-05-15 15:11:37 +0700

    lib(utils): add mapBoolean

--- lib/utils.ts
+++ lib/utils.ts
@@ -49,3 +49,48 @@ export function pickFirstLine(textdata: string) {
     }
     return extractedLines[0];
 }
+
+/**
+ * Map a string to a boolean, used for Express query.
+ * @param { any } input_data - data to map
+ * @returns { boolean } mapped boolean
+ */
+export function mapBoolean<T extends any>(input_data: T): boolean {
+    if (isNone(input_data)) {
+        return false;
+    }
+    let fstat = false;
+    let data: any;
+    if (typeof input_data === "string") {
+        data = input_data.toLowerCase() as string;
+    } else if (typeof input_data === "number") {
+        data = input_data.toString().toLowerCase() as string;
+    } else if (typeof input_data === "object") {
+        data = JSON.stringify(input_data);
+    } else if (typeof input_data === "boolean") {
+        return input_data;
+    } else {
+        // @ts-ignore
+        data = input_data.toString().toLowerCase();
+    }
+    switch (data) {
+        case "y":
+            fstat = true;
+            break;
+        case "enable":
+            fstat = true;
+            break;
+        case "true":
+            fstat = true;
+            break;
+        case "1":
+            fstat = true;
+            break;
+        case "yes":
+            fstat = true;
+            break;
+        default:
+            break;
+    }
+    return fstat;
+}
