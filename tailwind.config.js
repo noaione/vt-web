8dc79d2e1bab5c23460881596a2ee40e34374759
noaione <noaione0809@gmail.com>
2021-05-17 09:42:45 +0700

    tailwind: use trueGray

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
-const isProd = process.env.NODE_ENV === "production";
+const colors = require("tailwindcss/colors");
 
 module.exports = {
     mode: "jit",
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     darkMode: "class", // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                gray: colors.trueGray,
+            },
+        },
     },
     variants: {
-        extend: {},
+        extend: {
+            backgroundColor: ["checked"],
+            borderColor: ["checked"],
+        },
     },
-    plugins: [
-        require("@tailwindcss/aspect-ratio"),
-        require("@tailwindcss/custom-forms")
-    ],
+    plugins: [require("@tailwindcss/aspect-ratio"), require("@tailwindcss/forms")],
 };
