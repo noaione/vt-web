dbb7891e7b3afac3aa5f2b8b8ee135a84998554e
noaione <noaione0809@gmail.com>
2021-06-03 15:28:14 +0700

    Add between pages loading indicator

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Router from "next/router";
+
+import ProgressBar from "@badrap/bar-of-progress";
 
 import "../styles/global.css";
 import type { AppProps } from "next/app";
+
+const progress = new ProgressBar({
+    size: 2,
+    color: "#3DCE70",
+    className: "z-[99]",
+    delay: 80,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
