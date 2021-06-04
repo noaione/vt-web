54ebb42becf1bd013c491ed753eb35afa8ed4772
noaione <noaione0809@gmail.com>
2021-06-04 08:38:27 +0700

    Use preact

--- next.config.js
+++ next.config.js
@@ -30,12 +30,33 @@ module.exports = {
     future: {
         webpack5: true,
     },
-    webpack: (config, { webpack }) => {
+    productionBrowserSourceMaps: true,
+    webpack: (config, { dev, isServer, webpack }) => {
         config.plugins.push(
             new webpack.ProvidePlugin({
                 React: "react",
             })
         );
+
+        if (!isServer) {
+            Object.assign(config.resolve.alias, {
+                react: "preact/compat",
+                "react-dom/test-utils": "preact/test-utils",
+                "react-dom": "preact/compat",
+                "react-ssr-prepass": "preact-ssr-prepass",
+            });
+        }
+
+        const prependToEntry = isServer ? "pages/_document" : "main.js";
+        const preactDebug = dev ? ["preact/debug"] : ["preact/devtools"];
+
+        const entry = config.entry;
+        config.entry = () => {
+            return entry().then((entries) => {
+                entries[prependToEntry] = preactDebug.concat(entries[prependToEntry] || []);
+                return entries;
+            });
+        };
 
         useESBuildMinify(config);
         useESBuildLoader(config, {
