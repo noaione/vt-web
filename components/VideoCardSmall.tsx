0bb1d43b35c826be59e4f17cf7ec88dc8bcda4f8
noaione <noaione0809@gmail.com>
2021-06-29 19:32:47 +0700

    VideoCardSmall: dont prefetch automatically

--- components/VideoCardSmall.tsx
+++ components/VideoCardSmall.tsx
@@ -83,7 +83,11 @@ export default class VideoCardSmall extends React.Component<VideoCardProps> {
                             {viewersJSX}
                         </div>
                         <div className="my-2 mx-2">
-                            <Link href={`/video/${platformToShortCode(platform)}-${id}`} passHref>
+                            <Link
+                                href={`/video/${platformToShortCode(platform)}-${id}`}
+                                prefetch={false}
+                                passHref
+                            >
                                 <a className="text-sm uppercase tracking-wide text-blue-400 hover:text-blue-300 duration-200 transition-colors">
                                     Info
                                 </a>
