ec5d5e3a4beb3986e3577737b17dcb8540b434a2
noaione <noaione0809@gmail.com>
2021-07-06 17:31:31 +0700

    typing(next): remove extended global

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,14 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
-
-declare global {
-    namespace NodeJS {
-        // Extend process.env typing
-        interface ProcessEnv {
-            TOKEN_SECRET?: string;
-            HASHED_WEB_PASSWORD?: string;
-            IHAAPI_PASSWORD?: string;
-        }
-    }
-}
