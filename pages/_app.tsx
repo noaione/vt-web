509e51ea9b90007b29476a6d30320d0f479e16d4
noaione <noaione0809@gmail.com>
2021-07-06 17:30:51 +0700

    pages(app): add the provider context for react-redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Router from "next/router";
+import { Provider } from "react-redux";
 
 import ProgressBar from "@badrap/bar-of-progress";
 
 import "../styles/global.css";
 import type { AppProps } from "next/app";
+import { store } from "../lib/store";
 
 const progress = new ProgressBar({
     size: 2,
@@ -17,8 +19,12 @@ Router.events.on("routeChangeStart", progress.start);
 Router.events.on("routeChangeComplete", progress.finish);
 Router.events.on("routeChangeError", progress.finish);
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+function VTWebApp({ Component, pageProps }: AppProps) {
+    return (
+        <Provider store={store}>
+            <Component {...pageProps} />
+        </Provider>
+    );
 }
 
-export default MyApp;
+export default VTWebApp;
