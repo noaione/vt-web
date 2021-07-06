bd0d77a89a0a16b75f08abe41c8dc38d6f226ddb
noaione <noaione0809@gmail.com>
2021-07-06 19:28:49 +0700

    chore(store): reorder import

--- lib/store.ts
+++ lib/store.ts
@@ -1,4 +1,4 @@
-import { Action, configureStore, ThunkAction, applyMiddleware } from "@reduxjs/toolkit";
+import { Action, applyMiddleware, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
