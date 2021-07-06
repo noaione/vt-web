8c2415be57a220871eb6a969afe516f36c488816
noaione <noaione0809@gmail.com>
2021-07-06 17:29:57 +0700

    SettingsComponent: add LivesSort

--- components/SettingsComponents/index.tsx
+++ components/SettingsComponents/index.tsx
@@ -1,11 +1,13 @@
 import ExcludeComponents from "./Excluder";
 import FreeChatIncludeSettings from "./FreeChat";
+import LivesSortSettings from "./LivesSort";
 import PlatformComponent from "./Platform";
 import TimezoneSettings from "./Timezone";
 
 const SettingsComponent = {
     Excluder: ExcludeComponents,
     FreeChat: FreeChatIncludeSettings,
+    LivesSort: LivesSortSettings,
     Platform: PlatformComponent,
     Timezone: TimezoneSettings,
 };
