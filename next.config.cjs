8831ce37334c0332964ec2040b7f6d5dc2528246
noaione <noaione0809@gmail.com>
2021-10-20 22:51:36 +0800

    Upgrade nextjs to use swc and more

---
+++ next.config.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+    productionBrowserSourceMaps: true,
+    experimental: {
+        swcLoader: true,
+        swcMinify: true,
+        esmExternals: true,
+    },
+    webpack: (config, { dev, isServer, webpack }) => {
+        config.plugins.push(
+            new webpack.ProvidePlugin({
+                React: "react",
+            })
+        );
+
+        if (!isServer && !dev) {
+            Object.assign(config.resolve.alias, {
+                react: "preact/compat",
+                "react-dom/test-utils": "preact/test-utils",
+                "react-dom": "preact/compat",
+                "react-ssr-prepass": "preact-ssr-prepass",
+            });
+        }
+
+        const prependToEntry = isServer ? "pages/_document" : "main.js";
+        const preactDebug = dev ? ["preact/debug"] : ["preact/devtools"];
+
+        const entry = config.entry;
+        config.entry = () => {
+            return entry().then((entries) => {
+                entries[prependToEntry] = preactDebug.concat(entries[prependToEntry] || []);
+                return entries;
+            });
+        };
+
+        return config;
+    },
+    publicRuntimeConfig: {
+        TOKEN_SECRET: process.env.TOKEN_SECRET,
+    },
+};
